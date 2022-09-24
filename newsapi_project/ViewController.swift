96511fac8d70d8b78bd02687fc172d54500d9e9b
Ardyan <rizki.ardyan@sekolahmu.co.id>
2022-09-24 20:31:43 +0700

    Initial Commit

---
+++ newsapi_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newsapi_project
+//
+//  Created by Ardyan on 24/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
